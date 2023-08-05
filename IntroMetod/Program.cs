40366c9fe07e0b802490fce69e36f38a901488d9
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-06 00:41:45 +0300

    finished the IntroMetod

---
+++ IntroMetod/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+   int result = arg1;
+   if(arg2 > result) result = arg2;
+   if(arg3 > result) result = arg3;
+   return result;
+}
+
+int a1 = 1;
+int a2 = 2;
+int a3 = 3;
+int a4 = 4;
+int a5 = 5;
+int a6 = 6;
+int a7 = 157;
+int a8 = 8;
+int a9 = 7;
+
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(a4, a5, a6);
+// int max3 = Max(a7, a8, a9);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+   Max(a1, a2, a3),
+   Max(a4, a5, a6),
+   Max(a7,a8,a9));
+
+Console.WriteLine(max);
